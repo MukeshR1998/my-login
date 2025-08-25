66322ff99476b47f3f66c52a75afe0c2a3bc0d09
unknown <mukesh.rajbanshi1998@gmail.com>
2025-08-25 16:16:34 +0545

    first commit

---
+++ components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col gap-2 w-full justify-center items-center h-screen">
+      <input
+        className="bg-gray-500 rounded px-2"
+        type="email"
+        placeholder="email"
+      />
+      <input
+        className="bg-gray-500 rounded px-2"
+        type="password"
+        placeholder="password"
+      />
+      <button className="border border-green-500 rounded px-3">Login</button>
+    </div>
+  );
+};
+
+export default Login;
